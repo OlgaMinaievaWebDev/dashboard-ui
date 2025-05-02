498d86d1e9a0ff3040e25543cdd76a00fc1e8045
Olga <minaeva9@gmail.com>
2025-05-01 20:12:25 -0400

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path='/account' element={<Account/>}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+ return (
+  <header>
+   <h1>Hi Arthur, welcome! You have 12 open tasks.</h1>
+   <input
+    type="text"
+    placeholder="Search..."
+    className="search" />
+   <span>AR</span>
+  </header>
+ )
+}
+
+export default Header
