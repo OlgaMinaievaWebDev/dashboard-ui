76cba07bb47f36b8f26fcd651f1c963fcbe84e9d
Olga <minaeva9@gmail.com>
2025-05-02 15:40:13 -0400

    layout update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 function Layout() {
  return (
-   <div className="min-h-screen flex flex-col bg-red-700 text-white  px-12 py-6">
+   <div className="min-h-screen flex flex-col bg-oxford-blue text-white  px-12 py-6">
      <Header />
      <div className="px-4 py-2">
        <Navbar />
