aaeadd2e567e0e32ff762a657c59cd8443d8a008
Olga <minaeva9@gmail.com>
2025-05-04 12:36:16 -0400

    hover effect added

--- src/components/quickActions/Tabs.jsx
+++ src/components/quickActions/Tabs.jsx
@@ -4,7 +4,7 @@ export default function Tabs({ tabs}) {
       {tabs.map((tab) => (
         <button
         key={tab.label} 
-        className="bg-azure text-white rounded-3xl text-md px-4 py-2">
+        className="bg-azure text-white rounded-3xl text-md px-4 py-2 hover:bg-azure/80 transition duration-300 ease-in-out">
           {tab.label}
         </button>
       ))}
