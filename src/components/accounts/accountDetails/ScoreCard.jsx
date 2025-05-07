cfbcb4b73f7c10a86a407a4f7925cb7bd769d06b
Olga <minaeva9@gmail.com>
2025-05-07 11:51:11 -0400

    scoreCard styled

--- src/components/accounts/accountDetails/ScoreCard.jsx
+++ src/components/accounts/accountDetails/ScoreCard.jsx
@@ -1,9 +1,17 @@
 export function ScoreCard({ title, value, description }) {
   return (
-    <div className="bg-[#1E293B] p-4 rounded-xl">
-      <h2 className="text-sm text-gray-400">{title}</h2>
-      <p className="text-4xl font-bold">{value}</p>
-      <p className="text-blue-400 text-sm mt-1">{description}</p>
+    <div className="bg-[#1E293B] p-4 rounded-xl flex flex-col justify-between">
+      <h2 className="text-xl">{title}</h2>
+      <div className="flex items-center gap-2">
+        <p className="text-4xl font-bold">{value}</p>
+        <div className="flex items-center gap-1 text-azure justify-start text-sm mt-1 rounded-2xl border border-azure w-fit px-3 py-1">
+          <div className="rounded-full w-2 h-2 bg-azure"></div>
+          <div className="rounded-full w-2 h-2 bg-azure"></div>
+          <div className="rounded-full w-2 h-2 bg-azure"></div>
+          <div className="rounded-full w-2 h-2 bg-azure"></div>
+          {description}
+        </div>
+      </div>
     </div>
   );
 }
