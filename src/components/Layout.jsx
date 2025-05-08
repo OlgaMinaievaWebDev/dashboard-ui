31ecedc12c576ba88836121d7b077a5da120d3a7
Olga <minaeva9@gmail.com>
2025-05-08 13:27:47 -0400

    Navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Navbar from "./navbar/Navbar";
 
+
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-oxford-blue text-white px-12 py-6">
       <Header />
       <div className="px-4 py-2">
-        <Navbar />
+       <Navbar/>
       </div>
       <main className="flex-grow p-6">
         <Outlet />
