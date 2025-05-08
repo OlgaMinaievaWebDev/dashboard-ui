9963c2e7f893ea2d07fc928e315b5ea0648395df
Olga <minaeva9@gmail.com>
2025-05-08 13:26:19 -0400

    update layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from "react-router-dom"
-import Header from "./Header"
-import Navbar from "./navbar/Navbar"
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Navbar from "./navbar/Navbar";
 
 function Layout() {
- return (
-   <div className="min-h-screen flex flex-col bg-oxford-blue text-white  px-12 py-6">
-     <Header />
-     <div className="px-4 py-2">
-       <Navbar />
-     </div>
-     <main className="flex-grow p-6">
-       <Outlet />
-     </main>
-   </div>
- );
+  return (
+    <div className="min-h-screen flex flex-col bg-oxford-blue text-white px-12 py-6">
+      <Header />
+      <div className="px-4 py-2">
+        <Navbar />
+      </div>
+      <main className="flex-grow p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
