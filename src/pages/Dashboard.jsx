dab58673bcff48293d478a5dbb30852474e378ac
Olga <minaeva9@gmail.com>
2025-05-08 10:51:21 -0400

    tablet screen

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,41 +7,40 @@ import { AccountsPage } from "../components/dashboard/myAccounts/AccountsPage";
 
 function Dashboard() {
   return (
-    <div className="grid gap-4">
-      {/* First row */}
-      <div className="grid grid-cols-1 md:grid-cols-10 gap-4">
-        {/* Main content (60%) */}
-        <div className="md:col-span-6 bg-[#101827]  rounded-lg shadow-sm">
+    <div className="space-y-4">
+      {/* First Row */}
+      <div className="flex flex-col lg:grid lg:grid-cols-10 gap-4">
+        {/* Main content */}
+        <div className="bg-[#101827] rounded-lg shadow-sm lg:col-span-6">
           <WorkQueue />
         </div>
 
-        {/* Sidebar (40% total) */}
-        <div className="md:col-span-4 grid grid-cols-1 md:grid-cols-4 gap-4">
-          {/* Goals (20%) */}
-          <div className="md:col-span-2 h-full">
-            <div className="h-full rounded-lg  shadow-sm bg-[#101827]">
-              <PortfolioGoalsNivo />
-            </div>
+        {/* Sidebar */}
+        <div className="flex flex-col lg:grid lg:grid-cols-4 gap-4 lg:col-span-4">
+          {/* Goals */}
+          <div className="rounded-lg shadow-sm bg-[#101827] h-full lg:col-span-2">
+            <PortfolioGoalsNivo />
           </div>
 
-          {/* Quick actions stacked (20%) */}
-          <div className="md:col-span-2 flex flex-col gap-4">
-            <div className="rounded-lg shadow-sm flex-1 bg-[#101827]">
+          {/* Quick Actions and Market Intelligence */}
+          <div className="flex flex-col gap-4 lg:col-span-2">
+            <div className="rounded-lg shadow-sm bg-[#101827] flex-1">
               <QuickActions />
             </div>
-            <div className=" p-4 rounded-lg shadow-sm flex-1 bg-[#101827]">
+            <div className="p-4 rounded-lg shadow-sm bg-[#101827] flex-1">
               <MarketIntelligence />
             </div>
           </div>
         </div>
       </div>
 
-      {/* Second row - full width */}
-      <div className=" rounded-lg shadow-sm p-4">
+      {/* Second Row */}
+      <div className="lg:col-span-6 rounded-lg shadow-sm p-4 bg-[#101827]">
         <AccountsPage />
       </div>
     </div>
   );
 }
+
 
 export default Dashboard;
